291d369b14f6df4aebce0b18b006c148bf0aaab0
thespacedoctor <nothingbutdave@gmail.com>
2016-06-23 09:52:27 +0100

    x

--- docs/source/_api/conf.py
+++ docs/source/_api/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # breaker documentation build configuration file, created by
-# sphinx-quickstart on Thu Jun 23 09:47:31 2016.
+# sphinx-quickstart on Thu Jun 23 09:52:14 2016.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
