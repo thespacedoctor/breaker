e5ef73b91c4bf586a97141f9bdee3069c3d89aed
thespacedoctor <nothingbutdave@gmail.com>
2016-12-06 10:37:48 +0000

    binning of FITS skymaps to reduce image size and docs brought up to date

--- docs/source/_api/conf.py
+++ docs/source/_api/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # breaker documentation build configuration file, created by
-# sphinx-quickstart on Wed Nov 30 09:36:17 2016.
+# sphinx-quickstart on Tue Dec  6 10:34:57 2016.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
