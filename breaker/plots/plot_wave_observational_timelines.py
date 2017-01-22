4abf38bc3f864a9b1f7bd395f2055fd9f1973610
thespacedoctor <nothingbutdave@gmail.com>
2017-01-22 20:54:25 +0000

    filenaming fix

--- breaker/plots/plot_wave_observational_timelines.py
+++ breaker/plots/plot_wave_observational_timelines.py
@@ -1569,12 +1569,12 @@ class plot_wave_observational_timelines():
             timeLimitLabels = ["21 days pre-detection", "in First 3 Days",
                                "Between 3-10 Days", "Between 10-17 Days", "Between 17-24 Days", "Between 24-31 Days", "> 31 Days", "no limit"]
             timeLimitDays = [(-21, 0), (0, 3), (3, 10), (10, 17),
-                             (17, 24), (24, 31), (31, 0)]
+                             (17, 24), (24, 31), (31, 0), (0, 0)]
         else:
             timeLimitLabels = ["in First 3 Days",
                                "Between 3-10 Days", "Between 10-17 Days", "Between 17-24 Days", "Between 24-31 Days", "> 31 Days"]
             timeLimitDays = [(0, 3), (3, 10), (10, 17),
-                             (17, 24), (24, 31), (31, 0), (0, 0)]
+                             (17, 24), (24, 31), (31, 0)]
         # timeLimitLabels = ["in First 3 Days"]
         # timeLimitDays = [(0, 3)]
         raLimits = [134.25, 144.75, 152.25, 159.50, 167.0, 174.5]
